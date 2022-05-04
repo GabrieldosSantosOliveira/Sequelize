cb3f8b9d8d9a4514ae8e0b2af3185d14f5f8e430
Gabriel Dos <86084272+GabrieldosSantosOliveira@users.noreply.github.com>
2022-05-04 21:14:54 +0000

    Feito os métodos get, delete, put, post

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 let express = require("express");
 let app = express();
 let {usuario} = require("./models")
+
 app.use(express.json());
+
 app.use(express.urlencoded({
   extended: true
 }))
+
+app.get('/:id', async function(req,res){
+let resultado = await usuario.findByPk(req.params.id);
+  res.json(resultado);
+})
 app.get('/', async function(req,res){
 let resultado = await usuario.findAll();
   res.json(resultado);
 })
 app.post('/', async function(req,res){
- let resultado = await usuario.create();
+ let resultado = await usuario.create(req.body);
+  res.json(resultado);
+})
+app.put('/:id', async function(req,res){
+  
+ let resultado = await usuario.update( 
+   req.body, 
+   {where: req.params} 
+) 
+   res.json(resultado) 
+})
+app.delete('/:id', async function(req,res){
+ let resultado = await usuario.destroy({ where: req.params});
   res.json(resultado);
 })
 app.listen(3000, function(){
